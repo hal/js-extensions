383cf42cd85a670cee2a527538c72c873dc0411c
Harald Pehl <harald.pehl@gmail.com>
2017-05-17 07:20:31 +0200

    Replace function with lambda

--- whoami/extension.js
+++ whoami/extension.js
@@ -1,23 +1,20 @@
 (function () {
 
     let core = hal.core.Core.getInstance();
-    let whoami = hal.core.Extension.header("whoami", "Who am I?",
-
-        function () {
-            let operation = core.operation(hal.dmr.ResourceAddress.root(), "whoami")
-                .param("verbose", true)
-                .build();
-            core.dispatcher.execute(operation, result => {
-                let username = hal.dmr.ModelNodeHelper.failSafeGet(result, "identity/username").asString();
-                let realm = hal.dmr.ModelNodeHelper.failSafeGet(result, "identity/realm").asString();
-                let roles = hal.dmr.ModelNodeHelper.failSafeList(result, "mapped-roles")
-                    .map(role => role.asString())
-                    .join(", ");
-                alert(`You are user ${username} at ${realm}.
+    let whoami = hal.core.Extension.header("whoami", "Who am I?", () => {
+        let operation = core.operation(hal.dmr.ResourceAddress.root(), "whoami")
+            .param("verbose", true)
+            .build();
+        core.dispatcher.execute(operation, result => {
+            let username = hal.dmr.ModelNodeHelper.failSafeGet(result, "identity/username").asString();
+            let realm = hal.dmr.ModelNodeHelper.failSafeGet(result, "identity/realm").asString();
+            let roles = hal.dmr.ModelNodeHelper.failSafeList(result, "mapped-roles")
+                .map(role => role.asString())
+                .join(", ");
+            alert(`You are user ${username} at ${realm}.
 Mapped roles: [${roles}].`);
-            });
         });
-
+    });
     core.extensionRegistry.register(whoami);
 
 }());
